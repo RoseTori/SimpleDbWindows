39dcd327637bd905f962786fc983904afc15cb73
RoseTori <cojdanil00@gmail.com>
2026-03-08 09:27:17 +0900

    Added insert and select statements

--- SimpleDbWindows/SimpleDb.cpp
+++ SimpleDbWindows/SimpleDb.cpp
@@ -55,6 +55,57 @@ void close_input_buffer(InputBuffer* input_buffer) {
 	free(input_buffer);
 }
 
+enum MetaCommanResult {
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNIZED_COMMAND
+};
+
+enum PrepareResult {
+	PREPARE_SUCCESS,
+	PREPARE_UNRECOGNIZED_STATEMENT
+};
+
+MetaCommanResult do_meta_command(InputBuffer* input_buffer) {
+	if (strcmp(input_buffer->buffer, ".exit") == 0) {
+		exit(EXIT_SUCCESS);
+	}
+	else {
+		return META_COMMAND_UNRECOGNIZED_COMMAND;
+	}
+}
+
+enum StatementType {
+	STATEMENT_INSERT,
+	STATEMENT_SELECT
+};
+
+struct Statement {
+	StatementType type;
+};
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+	if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+		statement->type = STATEMENT_INSERT;
+		return PREPARE_SUCCESS;
+	}
+	if (strcmp(input_buffer->buffer, "select") == 0) {
+		statement->type = STATEMENT_INSERT;
+		return PREPARE_SUCCESS;
+	}
+	return PREPARE_UNRECOGNIZED_STATEMENT;
+
+ }
+
+void execute_statement(Statement* statement) {
+	switch (statement->type) {
+		case (STATEMENT_INSERT):
+			std::cout << "This is where we could do an instert.\n";
+			break;
+		case (STATEMENT_SELECT):
+			std::cout << "This is where we could do a select.\n";
+			break;
+	}
+}
 
 
 
@@ -64,14 +115,29 @@ int main(int argc, char* argv[]) {
 		print_prompt();
 		read_input(input_buffer);
 
+		if (input_buffer->buffer[0] == '.') {
+			switch (do_meta_command(input_buffer)) {
+			case (META_COMMAND_SUCCESS):
+				continue;
+			case (META_COMMAND_UNRECOGNIZED_COMMAND):
+				std::cout << "Unrecognized Command " << input_buffer->buffer << "\n";
+				continue;
+			}
+		}
 
-		if (strcmp(input_buffer->buffer, ".exit") == 0) {
-			close_input_buffer(input_buffer);
-			exit(EXIT_SUCCESS);
+		Statement statement;
+		switch (prepare_statement(input_buffer, &statement)) {
+			case (PREPARE_SUCCESS):
+				break;
+			case (PREPARE_UNRECOGNIZED_STATEMENT):
+				std::cout << "Unrecognized keyword at start of: " << input_buffer->buffer << "\n";
+				continue;
 		}
-		else {
-			std::cout << "Unrecognized command: " << input_buffer->buffer << std::endl;
-		}
+
+		execute_statement(&statement);
+		std::cout << "Executed.\n";
 	}
+
+
 
 }
